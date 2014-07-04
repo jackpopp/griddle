793686e0212c0f05ea7fa005d8ae2152692ee2c9
jackpopp <jack.popp@hotmail.com>
2014-07-04 10:08:18 +0100

    Added autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('css', function(cb){
 	// compile sass
 	compile = gulp.src(ASSET_DIR+'css/**/*.sass')
 		.pipe(sass({sourcemap: true}))
+		.pipe(autoprefixer("last 7 version", "ie 8"))
 		.pipe(gulp.dest(DEV_DEST_DIR+'css'));
 
 	copy = gulp.src(ASSET_DIR+'css/**/*.css')
